65e0078613936f469dd529b5e501e8f09397fe9f
Charlotte <charlotte_toussaint@hotmail.com>
2018-04-19 13:46:11 +0200

    first commit

--- controllers/postController.php
+++ controllers/postController.php
@@ -1,33 +1,81 @@
 <?php
 
+//https://laravel.com/docs/5.6/controllers#resource-controllers
+// TABLEAU LARAVEL URL UP
 
-$triedLetter = (ctype_alpha($_POST['triedLetter']) && strlen($_POST['triedLetter']) === 1) ? $_POST['triedLetter'] : '';
+// Index = la liste des posts/ressources en GET
+function index()
+{
+    include 'models/post.php';
+    $posts = getAllPosts();
+    return [
+        'view' => 'postindex.php',
+        'data' => ['pageTitle' => 'liste des posts',
+                    'post' => $posts
+                  ]// Data est toujours un array.
+    ];
+}
+
+// Create = Ca affiche le formulaire de création pour une ressource en GET
+function create(){
+    return [
+        'view' => 'postcreate.php',
+        'data' => [
+        ]
+    ];
+}
 
 
-if (!$triedLetter) {
-    header('Location:/views/errors/wrong-letter.html');
-    exit;
-};
+// Elle stock la ressource  dans la base de données en POST
+function store()
+{
+    include'models/post.php';
+    $post = storePost($title,$contenu);
+    return [
+        'view' => 'index.php',
+        'data' => [
+            'pageTitle' => $post->title,
+            'post' => $post
+        ]
+    ];
+    header('Location: index.php?a=show&r=post&id=1');
 
-$_SESSION['triedLetters'] .= $triedLetter;
-$_SESSION['letters'][$triedLetter] = false;
-                    
-    $letterFound = false;
-for ($i = 0; $i < $_SESSION['wordLength']; $i++) {
-    $l = substr($_SESSION['word'], $i, 1);
-        if($triedLetter === $l){
-            $letterFound = true;
-            $_SESSION['replacementString'] = substr_replace($_SESSION['replacementString'], $l, $i, 1);
-        }
-    }
+}
 
-    if(!$letterFound){
-        $_SESSION['trials']++;
-    }else{
-        var_dump($_SESSION['word']);
-        if ($_SESSION['word'] === $_SESSION['replacementString']) {
-            $_SESSION['wordFound'] = true;
-        }   
-    }
+// Affiche une ressource par rapport à un "indice" GET/photo{2}
+function show()
+{
+    if (!isset($_GET['id']) || !ctype_digit($_GET['id'])) return false;
+    $id = $_GET['id'];
+    include 'models/post.php';
+    $post = getOnePost($id);
+    return [
+        'view' => 'postshow.php',
+        'data' => [
+            'pageTitle' => $post->title,
+            'post' => $post
+        ]
+    ];
+}
 
-$_SESSION['remainingTrials'] = MAX_TRIALS - $_SESSION['trials'];
+// Afficher le formulaire d'édition sur base d'un identifiant EN GET
+function edit($id)
+{
+    return [
+        'view' => 'postedit.php',
+        'data' => []
+    ];
+}
+
+// Modifier le post dans la base de données et par rapport à l'identifiant
+function update($id)
+{
+    header('Location: index.php?a=show&r=post&id=' . $id);
+}
+
+// Supprime le post de la base de données
+function destroyed($id)
+{
+    header('Location: index.php?a=show&r=post');
+}
+
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 include 'configs/config.php';
-include 'models/model.php';
-include 'controllers/controller.php';
-include 'views/view.php';
+include 'router.php';
+include 'views/layout.php';
 
